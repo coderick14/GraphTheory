f9049293fcadc40c16ef38fb3988bc7986441404
coderick14 <bhattacharyya.rick14@gmail.com>
2017-04-17 23:02:46 +0530

    Use struct to represent graph

--- Kahn'sAlgo_topo_bfs.cpp
+++ Kahn'sAlgo_topo_bfs.cpp
@@ -1,75 +1,54 @@
-#include <iostream>
-#include <list>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-class graph
-{
-    int v;
-    list<int> *adj;
-    public:
-    graph(int);
-    void addedge(int,int);
-    void topo_bfs();
+struct node {
+    bool visited;
+    vector<int> adj;
 };
-graph::graph(int v)
-{
-    this->v=v;
-    adj=new list<int>[v];
-}
-void graph::addedge(int a,int b)
-{
-    adj[a].push_back(b);
-}
-void graph::topo_bfs()
-{
-    int i,s,indeg[v];
-    list<int> q;
+
+void topological_sort(vector<node> &graph, int n) {
+    int i,j,s,u,v,indeg[n];
+    queue<int> q;
     vector<int> topo_order;
-    list<int>::iterator it;
-    for(i=0;i<v;i++)
+    for(i=0;i<n;i++)
         indeg[i]=0;
-    for(i=0;i<v;i++)
-    {
-        for(it=adj[i].begin();it!=adj[i].end();it++)
-            indeg[*it]+=1;
+    for(i=0;i<n;i++) {
+        for(j=0; j<graph[i].adj.size(); j++) {
+            v = graph[i].adj[j];
+            indeg[v]+=1;
+         }
     }
-    for(i=0;i<v;i++)
-    {
+    for(i=0;i<n;i++) {
         if(indeg[i]==0)
-            q.push_back(i);
+            q.push(i);
     }
-    while(!q.empty())
-    {
+    while(!q.empty()) {
         s=q.front();
-        q.pop_front();
+        q.pop();
         topo_order.push_back(s);
-        for(it=adj[s].begin();it!=adj[s].end();it++)
-        {
-            indeg[*it]-=1;
-            if(indeg[*it]==0)
-                q.push_back(*it);
+        for(i=0; i<graph[s].adj.size(); i++) {
+            v = graph[s].adj[i];
+            indeg[v]-=1;
+            if(indeg[v]==0)
+                q.push(v);
         }
     }
-    if(topo_order.size()!=v)
-        cout<<"The given graph is not a dag";
-    else
-    {
+    if(topo_order.size()!=n)
+        cout<<"The given graph is not a dag\n";
+    else {
         cout<<"The topological sort is "<<endl;
         for(i=0;i<topo_order.size();i++)
             cout<<topo_order[i]<<" ";
     }
 }
-int main()
-{
-	int i,v,e,m,n;
-	cin>>v>>e;
-	graph g(v);
-	for(i=0;i<e;i++)
-	{
-	    cin>>m>>n;
-	    g.addedge(m,n);
+int main() {
+	int i,n,e,u,v;
+	cin>>n>>e;
+	vector<node> graph(n);
+	for(i=0;i<e;i++) {
+	    cin>>u>>v;
+	    graph[u].adj.push_back(v);
 	}
-	g.topo_bfs();
+	topological_sort(graph, n);
 	return 0;
 }
